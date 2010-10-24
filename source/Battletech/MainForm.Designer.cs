476f3b9b8f2fef7e0cce56d8d618838787f8982b
rathon <rathon@hotmail.com>
2010-10-24 16:52:09 -0700

    a few more layout changes

--- source/Battletech/MainForm.Designer.cs
+++ source/Battletech/MainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TechnologyTab = new System.Windows.Forms.TabControl();
             this.MainSelectionTabPage = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -49,39 +49,46 @@
             this.ClanRadio = new System.Windows.Forms.RadioButton();
             this.InnerSphereRadio = new System.Windows.Forms.RadioButton();
             this.TonnageCombo = new System.Windows.Forms.ComboBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tabControl1.SuspendLayout();
+            this.technologyGroupBox = new System.Windows.Forms.GroupBox();
+            this.ArmorGroupBox = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.TechnologyTab.SuspendLayout();
             this.MainSelectionTabPage.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.technologyGroupBox.SuspendLayout();
+            this.ArmorGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // TechnologyTab
             // 
-            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Right;
-            this.tabControl1.Controls.Add(this.MainSelectionTabPage);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Controls.Add(this.tabPage5);
-            this.tabControl1.Controls.Add(this.tabPage6);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tabControl1.Location = new System.Drawing.Point(447, 0);
-            this.tabControl1.Multiline = true;
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(415, 839);
-            this.tabControl1.TabIndex = 0;
+            this.TechnologyTab.Alignment = System.Windows.Forms.TabAlignment.Right;
+            this.TechnologyTab.Controls.Add(this.MainSelectionTabPage);
+            this.TechnologyTab.Controls.Add(this.tabPage2);
+            this.TechnologyTab.Controls.Add(this.tabPage3);
+            this.TechnologyTab.Controls.Add(this.tabPage4);
+            this.TechnologyTab.Controls.Add(this.tabPage5);
+            this.TechnologyTab.Controls.Add(this.tabPage6);
+            this.TechnologyTab.Dock = System.Windows.Forms.DockStyle.Right;
+            this.TechnologyTab.Location = new System.Drawing.Point(447, 0);
+            this.TechnologyTab.Multiline = true;
+            this.TechnologyTab.Name = "TechnologyTab";
+            this.TechnologyTab.SelectedIndex = 0;
+            this.TechnologyTab.Size = new System.Drawing.Size(415, 839);
+            this.TechnologyTab.TabIndex = 0;
             // 
             // MainSelectionTabPage
             // 
-            this.MainSelectionTabPage.Controls.Add(this.groupBox1);
+            this.MainSelectionTabPage.Controls.Add(this.technologyGroupBox);
             this.MainSelectionTabPage.Controls.Add(this.TonnageCombo);
             this.MainSelectionTabPage.Location = new System.Drawing.Point(4, 4);
             this.MainSelectionTabPage.Name = "MainSelectionTabPage";
             this.MainSelectionTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.MainSelectionTabPage.Size = new System.Drawing.Size(389, 831);
+            this.MainSelectionTabPage.Size = new System.Drawing.Size(388, 831);
             this.MainSelectionTabPage.TabIndex = 0;
-            this.MainSelectionTabPage.Text = "tabPage1";
+            this.MainSelectionTabPage.Text = "Technology";
             this.MainSelectionTabPage.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -89,9 +96,9 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 4);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(389, 831);
+            this.tabPage2.Size = new System.Drawing.Size(388, 831);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Engine";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // textBox1
@@ -173,12 +180,13 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.ArmorGroupBox);
             this.tabPage3.Location = new System.Drawing.Point(4, 4);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(389, 831);
+            this.tabPage3.Size = new System.Drawing.Size(388, 831);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.Text = "Armor";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
@@ -186,9 +194,9 @@
             this.tabPage4.Location = new System.Drawing.Point(4, 4);
             this.tabPage4.Name = "tabPage4";
             this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage4.Size = new System.Drawing.Size(389, 831);
+            this.tabPage4.Size = new System.Drawing.Size(388, 831);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.Text = "Weapons";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
@@ -196,7 +204,7 @@
             this.tabPage5.Location = new System.Drawing.Point(4, 4);
             this.tabPage5.Name = "tabPage5";
             this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage5.Size = new System.Drawing.Size(389, 831);
+            this.tabPage5.Size = new System.Drawing.Size(388, 831);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "tabPage5";
             this.tabPage5.UseVisualStyleBackColor = true;
@@ -206,7 +214,7 @@
             this.tabPage6.Location = new System.Drawing.Point(4, 4);
             this.tabPage6.Name = "tabPage6";
             this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage6.Size = new System.Drawing.Size(389, 831);
+            this.tabPage6.Size = new System.Drawing.Size(388, 831);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "tabPage6";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -214,7 +222,7 @@
             // ClanRadio
             // 
             this.ClanRadio.AutoSize = true;
-            this.ClanRadio.Location = new System.Drawing.Point(6, 42);
+            this.ClanRadio.Location = new System.Drawing.Point(6, 19);
             this.ClanRadio.Name = "ClanRadio";
             this.ClanRadio.Size = new System.Drawing.Size(46, 17);
             this.ClanRadio.TabIndex = 0;
@@ -225,7 +233,7 @@
             // InnerSphereRadio
             // 
             this.InnerSphereRadio.AutoSize = true;
-            this.InnerSphereRadio.Location = new System.Drawing.Point(58, 42);
+            this.InnerSphereRadio.Location = new System.Drawing.Point(58, 19);
             this.InnerSphereRadio.Name = "InnerSphereRadio";
             this.InnerSphereRadio.Size = new System.Drawing.Size(86, 17);
             this.InnerSphereRadio.TabIndex = 1;
@@ -255,23 +263,80 @@
             "90",
             "95",
             "100"});
-            this.TonnageCombo.Location = new System.Drawing.Point(13, 178);
+            this.TonnageCombo.Location = new System.Drawing.Point(6, 55);
             this.TonnageCombo.Name = "TonnageCombo";
             this.TonnageCombo.Size = new System.Drawing.Size(121, 21);
             this.TonnageCombo.TabIndex = 2;
             this.TonnageCombo.Text = "Select Tonnage";
             // 
-            // groupBox1
+            // technologyGroupBox
             // 
-            this.groupBox1.Controls.Add(this.ClanRadio);
-            this.groupBox1.Controls.Add(this.InnerSphereRadio);
-            this.groupBox1.Location = new System.Drawing.Point(6, 8);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(377, 65);
-            this.groupBox1.TabIndex = 12;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Technology Base:";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.technologyGroupBox.Controls.Add(this.ClanRadio);
+            this.technologyGroupBox.Controls.Add(this.InnerSphereRadio);
+            this.technologyGroupBox.Location = new System.Drawing.Point(6, 8);
+            this.technologyGroupBox.Name = "technologyGroupBox";
+            this.technologyGroupBox.Size = new System.Drawing.Size(377, 41);
+            this.technologyGroupBox.TabIndex = 12;
+            this.technologyGroupBox.TabStop = false;
+            this.technologyGroupBox.Text = "Technology Base:";
+            this.technologyGroupBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // ArmorGroupBox
+            // 
+            this.ArmorGroupBox.Controls.Add(this.radioButton4);
+            this.ArmorGroupBox.Controls.Add(this.radioButton3);
+            this.ArmorGroupBox.Controls.Add(this.radioButton2);
+            this.ArmorGroupBox.Controls.Add(this.radioButton1);
+            this.ArmorGroupBox.Location = new System.Drawing.Point(6, 8);
+            this.ArmorGroupBox.Name = "ArmorGroupBox";
+            this.ArmorGroupBox.Size = new System.Drawing.Size(376, 110);
+            this.ArmorGroupBox.TabIndex = 0;
+            this.ArmorGroupBox.TabStop = false;
+            this.ArmorGroupBox.Text = "groupBox2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(156, 20);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(156, 55);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -289,14 +354,17 @@
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.TechnologyTab);
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
+            this.TechnologyTab.ResumeLayout(false);
             this.MainSelectionTabPage.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.technologyGroupBox.ResumeLayout(false);
+            this.technologyGroupBox.PerformLayout();
+            this.ArmorGroupBox.ResumeLayout(false);
+            this.ArmorGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -304,7 +372,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl TechnologyTab;
         private System.Windows.Forms.TabPage MainSelectionTabPage;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
@@ -325,7 +393,12 @@
         private System.Windows.Forms.ComboBox TonnageCombo;
         private System.Windows.Forms.RadioButton InnerSphereRadio;
         private System.Windows.Forms.RadioButton ClanRadio;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox technologyGroupBox;
+        private System.Windows.Forms.GroupBox ArmorGroupBox;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
